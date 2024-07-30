b8592e887700bd325c06784101caf443cddb955b
Nicholas chuks <niccobery91@gmail.com>
2024-07-30 12:48:24 +0100

    created the hero section and made it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Button from "./components/Button";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -6,13 +6,14 @@ const Section = ({
   id,
   crosses,
   crossesOffset,
-  customPadding,
+  customPaddings,
   children,
 }) => {
   return (
     <div
+      id={id}
       className={`relative ${
-        customPadding ||
+        customPaddings ||
         `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
           className || ""
         }`
